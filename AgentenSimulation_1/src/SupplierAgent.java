716a8453f80b49317529b0c6b688b88ef31eaa6e
MidFidelity <32399022+MidFidelity@users.noreply.github.com>
2024-03-18 16:58:00 +0100

    UPDATE: - Implement VoteLoop for SupplierAgent

--- AgentenSimulation_1/src/SupplierAgent.java
+++ AgentenSimulation_1/src/SupplierAgent.java
@@ -1,65 +1,94 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SupplierAgent extends Agent {
 
-	private int[][] costMatrix;
+    private int[][] costMatrix;
 
-	public SupplierAgent(File file) throws FileNotFoundException {
+    public SupplierAgent(File file) throws FileNotFoundException {
 
-		Scanner scanner = new Scanner(file);
-		int dim = scanner.nextInt();
-		costMatrix = new int[dim][dim];
-		for (int i = 0; i < dim; i++) {
-			for (int j = 0; j < dim; j++) {
-				int x = scanner.nextInt();
-				costMatrix[i][j] = x;
-			}
-		}
-		scanner.close();
-	}
+        Scanner scanner = new Scanner(file);
+        int dim = scanner.nextInt();
+        costMatrix = new int[dim][dim];
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                int x = scanner.nextInt();
+                costMatrix[i][j] = x;
+            }
+        }
+        scanner.close();
+    }
 
-	public boolean vote(int[] contract, int[] proposal) {
-		int costContract = evaluate(contract);
-		int costProposal = evaluate(proposal);
-		if (costProposal < costContract)
-			return true;
-		else
-			return false;
-	}
+    public boolean vote(int[] contract, int[] proposal) {
+        int costContract = evaluate(contract);
+        int costProposal = evaluate(proposal);
+        if (costProposal < costContract)
+            return true;
+        else
+            return false;
+    }
 
-	public int getContractSize() {
-		return costMatrix.length;
-	}
+    public int getContractSize() {
+        return costMatrix.length;
+    }
 
-	@Override
-	public boolean[] voteLoop(int[][] contracts, int acceptanceAmount) {
-		//TODO
-		return new boolean[0];
-	}
+    @Override
+    public boolean[] voteLoop(int[][] contracts, final int acceptanceAmount) {
+        Map<Integer, Integer> costs = new LinkedHashMap<>();
+        for (int i = 0; i < contracts.length; i++) {
+            costs.put(i, evaluate(contracts[i]));
+        }
 
-	@Override
-	public int voteEnd(int[][] contracts) {
-		//TODO
-		return 0;
-	}
+        costs = sortByValue(costs);
 
-	public void printUtility(int[] contract) {
-		System.out.print(evaluate(contract));
-	}
+        boolean[] result = new boolean[contracts.length];
+        AtomicInteger count = new AtomicInteger();
 
-	
-	private int evaluate(int[] contract) {
+        costs.forEach((integer, integer2) -> {
+            if (count.get() < acceptanceAmount) {
+                result[integer] = true;
+                count.getAndIncrement();
+            }
+        });
 
-		int result = 0;
-		for (int i = 0; i < contract.length - 1; i++) {
-			int zeile = contract[i];
-			int spalte = contract[i + 1];
-			result += costMatrix[zeile][spalte];
-		}
+        return result;
+    }
 
-		return result;
-	}
+    private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
+    @Override
+    public int voteEnd(int[][] contracts) {
+        //TODO
+        return 0;
+    }
+
+    public void printUtility(int[] contract) {
+        System.out.print(evaluate(contract));
+    }
+
+
+    private int evaluate(int[] contract) {
+
+        int result = 0;
+        for (int i = 0; i < contract.length - 1; i++) {
+            int zeile = contract[i];
+            int spalte = contract[i + 1];
+            result += costMatrix[zeile][spalte];
+        }
+
+        return result;
+    }
 
 }
