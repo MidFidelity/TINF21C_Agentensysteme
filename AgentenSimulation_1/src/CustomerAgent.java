a717c685c779a97f6400e87ae34dd82e4407ec41
Dana Frey <inf21099@lehre.dhbw-stuttgart.de>
2024-03-18 18:05:30 +0100

    UPDATE: CustomerAgent voteLoop(), voteEnd(), evaluateNEW()

--- AgentenSimulation_1/src/CustomerAgent.java
+++ AgentenSimulation_1/src/CustomerAgent.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CustomerAgent extends Agent {
 
 	private int[][] timeMatrix;
+	HashMap<int[], Integer> evaluatedCosts = new HashMap<>();
 
 	public CustomerAgent(File file) throws FileNotFoundException {
 
@@ -33,46 +34,93 @@ public class CustomerAgent extends Agent {
 			return false;
 	}
 
-	public boolean[] voteLoop (int[][] contracts, int acceptanceAmount){
-		int[][] clonedContracts = contracts;
-		boolean[] results = new boolean[contracts.length];
-		//iterate over all the rows and check if one contract is better than the other, if so the better contract moves to the left
-		//bubblesort (?)
-		for(int rowsA=0;rowsA<contracts.length;rowsA++) {
-			for(int rowsB=0;rowsB<contracts.length;rowsB++) {
-				if (vote(contracts[rowsB], contracts[rowsB + 1])) {
-					int[] temp = clonedContracts[rowsB];
-					clonedContracts[rowsB] = clonedContracts[rowsB + 1];
-					clonedContracts[rowsB + 1] = temp;
-				}
-				rowsB++;
-			}
-			rowsA++;
-		}
-		//after the best contracts have been moved to the top, an iteration (for the true/false determination) is needed to retain
-		//the order of the original contracts array
-		//e.g. if the array from our contracts[0] has been found among the first 60 arrays of our sorted/cloned array then -> write true for
-		//results[0], else write false; repeat for every row
-		for(int rows=0;rows<contracts.length;rows++) {
-			for(int rowsCloned=0;rowsCloned<acceptanceAmount;rowsCloned++) {
-				if(clonedContracts[rowsCloned] == contracts[rows]){
-					results[rows] = true;
-				}
-				rowsCloned++;
-			}
-			if(results[rows] != true){
-				results[rows] = false;
-			}
-			rows++;
+	public boolean[] voteLoop(int[][] contracts, final int acceptanceAmount) {
+		Map<Integer, Integer> costs = new LinkedHashMap<>();
+		for (int i = 0; i < contracts.length; i++) {
+			costs.put(i, evaluateNEW(contracts[i]));
 		}
 
-		return results;
+		costs = sortByValue(costs);
+
+		boolean[] result = new boolean[contracts.length];
+		AtomicInteger count = new AtomicInteger();
+
+		costs.forEach((integer, integer2) -> {
+			if (count.get() < acceptanceAmount) {
+				result[integer] = true;
+				count.getAndIncrement();
+			}
+		});
+
+		return result;
+	}
+
+//	public boolean[] voteLoop (int[][] contracts, int acceptanceAmount){
+//		int[][] clonedContracts = contracts;
+//		boolean[] results = new boolean[contracts.length];
+//		//iterate over all the rows and check if one contract is better than the other, if so the better contract moves to the left
+//		//bubblesort (?)
+//		for(int rowsA=0;rowsA<contracts.length;rowsA++) {
+//			for(int rowsB=0;rowsB<contracts.length;rowsB++) {
+//				if (vote(contracts[rowsB], contracts[rowsB + 1])) {
+//					int[] temp = clonedContracts[rowsB];
+//					clonedContracts[rowsB] = clonedContracts[rowsB + 1];
+//					clonedContracts[rowsB + 1] = temp;
+//				}
+//				rowsB++;
+//			}
+//			rowsA++;
+//		}
+//		//after the best contracts have been moved to the top, an iteration (for the true/false determination) is needed to retain
+//		//the order of the original contracts array
+//		//e.g. if the array from our contracts[0] has been found among the first 60 arrays of our sorted/cloned array then -> write true for
+//		//results[0], else write false; repeat for every row
+//		for(int rows=0;rows<contracts.length;rows++) {
+//			for(int rowsCloned=0;rowsCloned<acceptanceAmount;rowsCloned++) {
+//				if(clonedContracts[rowsCloned] == contracts[rows]){
+//					results[rows] = true;
+//				}
+//				rowsCloned++;
+//			}
+//			if(results[rows] != true){
+//				results[rows] = false;
+//			}
+//			rows++;
+//		}
+//
+//		return results;
+//	}
+
+	private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+		List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+		list.sort(Map.Entry.comparingByValue());
+
+		Map<K, V> result = new LinkedHashMap<>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+
+		return result;
 	}
 
 	@Override
 	public int voteEnd(int[][] contracts) {
-		//TODO
-		return 0;
+		//calculation of costs of given contracts
+		Map<Integer, Integer> costs = new LinkedHashMap<>();
+		for (int i = 0; i < contracts.length; i++) {
+			costs.put(i, evaluateNEW(contracts[i]));
+		}
+		//sort the costs
+		Map<Integer, Integer> sortedCosts = sortByValue(costs);
+		//find the index/key of the highest/worst cost in the costs map of ln 109
+		int foundKey = 0;
+		for (Map.Entry<Integer, Integer> entry : costs.entrySet()) {
+			if(entry.getValue().equals(sortedCosts.get(contracts.length-1))) {
+				foundKey = entry.getKey();
+				break;
+			}
+		}
+		return foundKey;
 	}
 
 
@@ -85,51 +133,59 @@ public class CustomerAgent extends Agent {
 	}
 	
 	private int evaluateNEW(int[] solution) {
-		int anzM = timeMatrix[0].length;
-		
-		if(timeMatrix.length != solution.length)System.out.println("Fehler in ");
-		int[][] start = new int[timeMatrix.length][timeMatrix[0].length];
+		if(evaluatedCosts.containsKey(solution))
+		{
+			return evaluatedCosts.get(solution);
+		}
+		else
+		{
+			int anzM = timeMatrix[0].length;
 
-		for(int i=0;i<start.length;i++) {
-			for(int j=0;j<start[i].length;j++) {
-				start[i][j] = 0;
-			}
-		}
-		
-		int job = solution[0];
-		for(int m=1;m<anzM;m++) {
-			start[job][m] = start[job][m-1] + timeMatrix[job][m-1];
-		}
-		
-		for(int j=1;j<solution.length;j++) {
-			int delay             = 0;
-			int vorg              = solution[j-1];
-			job                   = solution[j];
-			boolean delayErhoehen;  
-			do {
-				delayErhoehen = false;
-				start[job][0] = start[vorg][0] + timeMatrix[vorg][0] + delay;
-				for(int m=1;m<anzM;m++) {					
-					start[job][m] = start[job][m-1] + timeMatrix[job][m-1];
-					if(start[job][m] < start[vorg][m]+timeMatrix[vorg][m]) {
-						delayErhoehen = true;
-						delay++;
-						break;
-					}
+			if(timeMatrix.length != solution.length)System.out.println("Fehler in ");
+			int[][] start = new int[timeMatrix.length][timeMatrix[0].length];
+
+			for(int i=0;i<start.length;i++) {
+				for(int j=0;j<start[i].length;j++) {
+					start[i][j] = 0;
 				}
-			}while(delayErhoehen);
-		}
-		int last = solution[solution.length-1];
-		
-		
+			}
+
+			int job = solution[0];
+			for(int m=1;m<anzM;m++) {
+				start[job][m] = start[job][m-1] + timeMatrix[job][m-1];
+			}
+
+			for(int j=1;j<solution.length;j++) {
+				int delay             = 0;
+				int vorg              = solution[j-1];
+				job                   = solution[j];
+				boolean delayErhoehen;
+				do {
+					delayErhoehen = false;
+					start[job][0] = start[vorg][0] + timeMatrix[vorg][0] + delay;
+					for(int m=1;m<anzM;m++) {
+						start[job][m] = start[job][m-1] + timeMatrix[job][m-1];
+						if(start[job][m] < start[vorg][m]+timeMatrix[vorg][m]) {
+							delayErhoehen = true;
+							delay++;
+							break;
+						}
+					}
+				}while(delayErhoehen);
+			}
+			int last = solution[solution.length-1];
+
+
 //		for(int j=0;j<solution.length;j++) {
 //			for(int m=0;m<anzM;m++) {
 //				System.out.print(start[j][m] + "\t");
 //			}
 //			System.out.println();
 //		}
-		
-		return (start[last][anzM-1]+timeMatrix[last][anzM-1]);
+			int costValue = start[last][anzM-1]+timeMatrix[last][anzM-1];
+			evaluatedCosts.put(solution, costValue);
+			return (costValue);
+		}
 	}
 
 	
